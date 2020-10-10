143a7d54b47082a839e4f7c55deb6eda0edb96be
Sakshi Choudhary <choudharysakshi023@gmail.com>
2020-10-10 20:12:29 +0530

    created dashboard route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("./../middleware/auth");
 
+
+const Question = require("../models/Question");
+const Article = require("../models/Article");
+
+
 //@router     GET /index
 //@desc       Welcome Homepage
 router.get("/", ensureGuest, (req, res) => {
@@ -11,6 +16,51 @@ router.get("/", ensureGuest, (req, res) => {
 //@router     GET /dashboard
 //@desc       Open Dashboard when succesfully logged In
 router.get("/dashboard", ensureAuth, async (req, res) => {
-  res.render("dashboard");
-});
+    try {
+        const questions = await Question.find();
+        const question = questions.filter(
+          (element) => element.userId.toString() === req.user._id.toString()
+        );
+        let answers = [];
+        questions.forEach((question) => {
+          question.answers.forEach((element) => {
+            if (element.userId.toString() === req.user._id.toString()) {
+              let answer = {
+                question: question.question,
+                username: question.username,
+                userId: question.userId,
+                category: question.category,
+                subject: question.subject,
+                answer: element.content,
+                id: element.id,
+              };
+              answers = [...answers, answer];
+            }
+          });
+        });
+        const articles = await Article.find({ userId: req.user._id });
+        // console.log(articles);
+    
+        res.render("dashboard", {
+          name: req.user.displayName,
+          image: req.user.image,
+          userId: req.user._id,
+          category: req.user.category,
+          questions: question,
+          answers,
+          bookmarks: req.user.bookmarks,
+          articles,
+        });
+      } catch (error) {
+        console.error(error);
+        res.send("Server error");
+      }
+    });
+    
+
+
+
+
+
+
 module.exports = router;
