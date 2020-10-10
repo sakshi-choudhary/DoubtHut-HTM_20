e6eaed34506c40a66a2541e401d8a3d2e66224d6
Sakshi Choudhary <choudharysakshi023@gmail.com>
2020-10-10 20:56:49 +0530

    designed dashboard frontend

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("./../middleware/auth");
 
-
 const Question = require("../models/Question");
 const Article = require("../models/Article");
-
 
 //@router     GET /index
 //@desc       Welcome Homepage
@@ -16,51 +14,45 @@ router.get("/", ensureGuest, (req, res) => {
 //@router     GET /dashboard
 //@desc       Open Dashboard when succesfully logged In
 router.get("/dashboard", ensureAuth, async (req, res) => {
-    try {
-        const questions = await Question.find();
-        const question = questions.filter(
-          (element) => element.userId.toString() === req.user._id.toString()
-        );
-        let answers = [];
-        questions.forEach((question) => {
-          question.answers.forEach((element) => {
-            if (element.userId.toString() === req.user._id.toString()) {
-              let answer = {
-                question: question.question,
-                username: question.username,
-                userId: question.userId,
-                category: question.category,
-                subject: question.subject,
-                answer: element.content,
-                id: element.id,
-              };
-              answers = [...answers, answer];
-            }
-          });
-        });
-        const articles = await Article.find({ userId: req.user._id });
-        // console.log(articles);
-    
-        res.render("dashboard", {
-          name: req.user.displayName,
-          image: req.user.image,
-          userId: req.user._id,
-          category: req.user.category,
-          questions: question,
-          answers,
-          bookmarks: req.user.bookmarks,
-          articles,
-        });
-      } catch (error) {
-        console.error(error);
-        res.send("Server error");
-      }
+  try {
+    const questions = await Question.find();
+    const question = questions.filter(
+      (element) => element.userId.toString() === req.user._id.toString()
+    );
+    let answers = [];
+    questions.forEach((question) => {
+      question.answers.forEach((element) => {
+        if (element.userId.toString() === req.user._id.toString()) {
+          let answer = {
+            question: question.question,
+            username: question.username,
+            userId: question.userId,
+            category: question.category,
+            subject: question.subject,
+            answer: element.content,
+            id: element.id,
+          };
+          answers = [...answers, answer];
+        }
+      });
     });
-    
+    const articles = await Article.find({ userId: req.user._id });
+    // console.log(articles);
 
-
-
-
-
+    res.render("dashboard", {
+      name: req.user.displayName,
+      image: req.user.image,
+      userId: req.user._id,
+      category: req.user.category,
+      questions: question,
+      answers,
+      bookmarks: req.user.bookmarks,
+      articles,
+    });
+  } catch (error) {
+    console.error(error);
+    res.send("Server error");
+  }
+});
 
 module.exports = router;
