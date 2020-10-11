da0a92651e20975f770c0b1314a2c9594fce4738
Sakshi Choudhary <choudharysakshi023@gmail.com>
2020-10-11 09:32:24 +0530

    Procfile

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const MongoStore = require("connect-mongo")(session);
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 
+
 const port = process.env.PORT || 7781
 const app = express();
 
