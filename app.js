2cddc248a39eaadb37140559a2389e078390cd57
Sakshi Choudhary <choudharysakshi023@gmail.com>
2020-10-11 09:44:47 +0530

    str

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("./node_modules/express");
+const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
